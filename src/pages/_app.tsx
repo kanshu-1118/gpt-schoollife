0d2fbee913a6f51c13c5f358944cb6e576ccc457
下河寛宗 <kan.1118.shu@gmail.com>
2023-06-12 14:25:29 +0900

    header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Noto_Sans_JP } from 'next/font/google'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// import "ress"
+const notojp = Noto_Sans_JP({
+  weight: ["400", "500"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export default function App({Component,pageProps} : AppProps){
+
+
+  return (
+    <main className={notojp.className}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </main>
+  )
 }
+
---
+++ src/pages/menber.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from './components/header'
+
+const menber = () => {
+    return (
+        <Header />
+    )
+}
+
+export default menber
