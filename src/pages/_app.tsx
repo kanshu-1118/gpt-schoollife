bfcde0aeb8c85f72693b8b3d4925d541eaff8495
下河寛宗 <kan.1118.shu@gmail.com>
2023-06-19 14:53:04 +0900

    googleアナリティクス設置

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,7 @@
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Noto_Sans_JP } from 'next/font/google'
@@ -11,9 +15,36 @@ const notojp = Noto_Sans_JP({
 
 export default function App({Component,pageProps} : AppProps){
 
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
 
   return (
     <main className={notojp.className}>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+          `,
+        }}
+      />
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
