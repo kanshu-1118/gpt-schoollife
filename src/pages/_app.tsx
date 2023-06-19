30a4dffe94ed38042601691256237888fed85489
honnosouta <honnosouta1@gmail.com>
2023-06-19 15:47:33 +0900

    second commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+// import "../styles/style.scss"
+import "ress"
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,15 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: any, res: any) {
+  const APIKEY = `d885a2375ca7b09b`;
+  const shopname = "網場"
+  try {
+    const response = await 
+    fetch(`http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${APIKEY}&name=${shopname}&format=json`);
+    const data = await response.json(); // JSONデータを取得
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'API request failed' });
+  }
 }
